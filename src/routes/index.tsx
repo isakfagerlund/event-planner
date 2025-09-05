3045af6746902296152b881690a28a87e79dcf3c
Isak Fagerlund <isak@autarc.energy>
2025-09-05 22:37:00 +0200

    tanstack db working

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,27 @@
 import { createFileRoute } from '@tanstack/react-router';
 import '../App.css';
+import {
+  createCollection,
+  localStorageCollectionOptions,
+  useLiveQuery,
+} from '@tanstack/react-db';
+import z from 'zod';
+
+const eventsSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  createdAt: z.date(),
+  updatedAt: z.date(),
+});
+
+const eventsCollection = createCollection(
+  localStorageCollectionOptions({
+    id: 'events',
+    getKey: (event) => event.id,
+    schema: eventsSchema,
+    storageKey: 'app-events',
+  })
+);
 
 export const Route = createFileRoute('/')({
   component: App,
@@ -9,6 +31,63 @@ function App() {
   return (
     <div className="App">
       <h1>Event Planner</h1>
+      <Events />
+    </div>
+  );
+}
+
+function Events() {
+  const { data: events } = useLiveQuery((q) =>
+    q
+      .from({ event: eventsCollection })
+      .orderBy(({ event }) => event.createdAt, 'asc')
+  );
+
+  const deleteEvent = (id: string) => eventsCollection.delete(id);
+
+  return (
+    <div>
+      <p>Events</p>
+      <AddEvent />
+      <ul>
+        {events.map((event) => (
+          <li>
+            {event.name} <span onClick={() => deleteEvent(event.id)}>🛑</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function AddEvent() {
+  const addEvent = (eventName: string) => {
+    eventsCollection.insert({
+      id: crypto.randomUUID(),
+      name: eventName,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+  };
+
+  return (
+    <div>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          const formData = new FormData(event.currentTarget);
+          const eventName = formData.get('eventName') as string;
+          if (eventName) {
+            addEvent(eventName);
+            event.currentTarget.reset();
+          }
+        }}
+      >
+        <input name="eventName" className="border border-black"></input>
+        <button className="border px-2 border-black" type="submit">
+          Add event
+        </button>
+      </form>
     </div>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,15 @@ import { defineConfig } from 'vite';
 import viteReact from '@vitejs/plugin-react';
 import tanstackRouter from '@tanstack/router-plugin/vite';
 import { resolve } from 'node:path';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tanstackRouter({ autoCodeSplitting: true }), viteReact()],
+  plugins: [
+    tanstackRouter({ autoCodeSplitting: true }),
+    viteReact(),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
